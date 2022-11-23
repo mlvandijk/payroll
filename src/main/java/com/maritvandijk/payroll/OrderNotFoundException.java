b67daf713807e44f2b3d8e2dafcf098dc8c235af
marit.van.dijk <mlvandijk@gmail.com>
2022-11-23 16:25:35 +0100

    Fix OrderNotFoundException

--- src/main/java/com/maritvandijk/payroll/OrderNotFoundException.java
+++ src/main/java/com/maritvandijk/payroll/OrderNotFoundException.java
@@ -2,5 +2,6 @@ package com.maritvandijk.payroll;
 
 public class OrderNotFoundException extends RuntimeException {
     public OrderNotFoundException(Long id) {
+        super("Could not find order " + id);
     }
 }
